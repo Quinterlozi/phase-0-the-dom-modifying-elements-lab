66c85cae99be22bb55f57c3df98166a07ae9fdb2
Quinterlozi <jojobobo254@gmail.com>
2023-03-23 16:50:23 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+// remove the <main> element with id="main"
+document.querySelector("#main").remove();
+
+// create a new <h1> element with id="victory" and a message
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+// add the new <h1> element to the document body
+document.body.appendChild(newHeader);
